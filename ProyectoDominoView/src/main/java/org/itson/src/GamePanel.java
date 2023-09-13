5b58eb141ea781ec50b43fe9575f09ac09562141
OscarMinjarez <oscarminjarez@outlook.com>
2023-09-13 14:59:59 -0700

    Main loop terminado

--- ProyectoDominoView/src/main/java/org/itson/src/GamePanel.java
+++ ProyectoDominoView/src/main/java/org/itson/src/GamePanel.java
@@ -4,7 +4,9 @@
  */
 package org.itson.src;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import javax.swing.JPanel;
@@ -53,24 +55,88 @@ public class GamePanel extends JPanel implements Runnable {
     public void run() {
         this.init();
         
+        final double GAME_HERTZ = 60.0;
+        final double TIME_BEFORE_UPDATE = 1_000_000_000 / GAME_HERTZ;
+        
+        final int MUST_UPDATE_BEFORE_RENDER = 5;
+        
+        double lastUpdateTime = System.nanoTime();
+        double lastRenderTime;
+        
+        final double TARGET_FPS = 60;
+        final double TOTAL_TIME_BEFORE_RENDER = 1_000_000_000 / TARGET_FPS;
+        
+        int frameCount = 0;
+        int lastSecondTime = (int) (lastUpdateTime / 1_000_000_000);
+        int oldFrameCount = 0;
+        
         while (this.isRunning) {
-            this.update();
+            double now = System.nanoTime();
+            int updateCount = 0;
+            
+            while (((now - lastUpdateTime) > TIME_BEFORE_UPDATE) && (updateCount < MUST_UPDATE_BEFORE_RENDER)) {
+                this.input();
+                this.update();
+                
+                lastUpdateTime += TIME_BEFORE_UPDATE;
+                updateCount++;
+            }
+            
+            if ((now - lastUpdateTime) > TIME_BEFORE_UPDATE) {
+                lastUpdateTime = now - TIME_BEFORE_UPDATE;
+            }
+            
+            this.input();
             this.render();
             this.draw();
+            
+            lastRenderTime = now;
+            frameCount++;
+            
+            int thisSecond = (int) (lastUpdateTime / 1_000_000_000);
+            
+            if (thisSecond > lastSecondTime) {
+                if (frameCount != oldFrameCount) {
+                    System.out.println("NEW SECOND " + thisSecond + " " + frameCount);
+                    oldFrameCount = frameCount;
+                }
+                
+                frameCount = 0;
+                lastSecondTime = thisSecond;
+            }
+            
+            while (now - lastRenderTime < TOTAL_TIME_BEFORE_RENDER && now - lastUpdateTime < TIME_BEFORE_UPDATE) {
+                Thread.yield();
+                
+                try {
+                    Thread.sleep(1);
+                } catch (Exception e) {
+                    System.out.println("ERROR: " + e.getMessage());
+                }
+                
+                now = System.nanoTime();
+            }
         }
     }
-    
-    private int x = 0;
+
+    public void input() {
+        
+    }
     
     public void update() {
-        System.out.println(this.x++);
+        
     }
     
     public void render() {
-        
+        if (this.g != null) {
+            g.setColor(new Color(66, 134, 244));
+            g.fillRect(0, 0, this.width, this.height);
+        }
     }
     
     public void draw() {
-        
+        Graphics g2 = (Graphics2D) this.getGraphics();
+        g2.drawImage(this.image, 0, 0, this.width, this.height, null);
+        g2.dispose();
     }
 }
