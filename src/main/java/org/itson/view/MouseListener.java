336532eecd80982bf300ed922062356b20e74072
LuisReynoso233531 <luis.reynoso233531@potros.itson.edu.mx>
2023-09-26 17:50:47 -0700

    Enseña y guarda las coordenadas del mouse y de la ficha cuando esta se arrastra

--- src/main/java/org/itson/view/MouseListener.java
+++ src/main/java/org/itson/view/MouseListener.java
@@ -3,13 +3,26 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package org.itson.view;
+
+import java.awt.Point;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class MouseListener extends MouseAdapter implements MouseMotionListener {
+
     private static MouseListener instance;
+    private List<Point> mouseCoordinates = new ArrayList<>();
+
+    public List<Point> getMouseCoordinates() {
+        return mouseCoordinates;
+    }
+
+    public void addMouseCoordinate(Point coordinate) {
+        mouseCoordinates.add(coordinate);
+    }
 
     private MouseListener() {
     }
@@ -41,14 +54,33 @@ public class MouseListener extends MouseAdapter implements MouseMotionListener {
 
     @Override
     public void mouseDragged(MouseEvent e) {
+        int x = e.getX();
+        int y = e.getY();
+        Point coordinate = new Point(x, y);
+        addMouseCoordinate(coordinate);
+        System.out.println("Mouse X: " + x + ", Mouse Y: " + y);
+
         for (Sprite sprite : Display.getSprites()) {
             if (sprite.isDragging()) {
-                sprite.updatePosition(e.getX(), e.getY());
+                sprite.updatePosition(x, y);
             }
         }
     }
 
     @Override
     public void mouseMoved(MouseEvent e) {
+        int x = e.getX();
+        int y = e.getY();
+        Point coordinate = new Point(x, y);
+        addMouseCoordinate(coordinate);
+        System.out.println("Mouse X: " + x + ", Mouse Y: " + y);
     }
+
+    public void getCords() {
+        List<Point> coordinates = MouseListener.get().getMouseCoordinates();
+        for (Point coordinate : coordinates) {
+            System.out.println("X: " + coordinate.getX() + ", Y: " + coordinate.getY());
+        }
+    }
+
 }
