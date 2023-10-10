8f07d1c064d6126e2aaf4f122b918978d9e33079
LuisReynoso233531 <luis.reynoso233531@potros.itson.edu.mx>
2023-10-09 23:17:57 -0700

    Creacion de la private class Face

--- src/main/java/org/itson/model/domain/Token.java
+++ src/main/java/org/itson/model/domain/Token.java
@@ -12,55 +12,66 @@ import org.itson.view.Sprite;
  */
 public class Token extends Entity {
 
-   public int side1;
-   public int side2;
-   public Sprite sprite;
+    private Face[] faces;
     private Vector2 position; // Agrega una posición para el token
 
-    public Token(int side1, int side2, Sprite sprite) {
-        this.side1 = side1;
-        this.side2 = side2;
-        this.sprite = sprite;
-        this.position = new Vector2(0, 0); // Inicializa la posición en (0, 0) por defecto
+    public Token() {
+
     }
 
-   @Override
-    public void setPosition(Vector2 position) {
+    public Token(Vector2 position) {
+        this.faces = new Face[2];
         this.position = position;
     }
 
-   @Override
+    private class Face {
+
+        private Sprite sprite;
+        private int valor;
+
+        public Face(Sprite sprite, int valor) {
+            this.sprite = sprite;
+            this.valor = valor;
+        }
+
+        public Face() {
+
+        }
+
+        public Sprite getSprite() {
+            return sprite;
+        }
+
+        public void setSprite(Sprite sprite) {
+            this.sprite = sprite;
+        }
+
+        public int getValor() {
+            return valor;
+        }
+
+        public void setValor(int valor) {
+            this.valor = valor;
+        }
+    }
+
+    @Override
     public Vector2 getPosition() {
         return position;
     }
 
     public void draw(Graphics g, ImageObserver observer) {
-    sprite.draw(g, observer);
-    }
-    
-    public void setSprite(Sprite sprite) {
-    this.sprite = sprite;
+//        sprite.draw(g, observer);
     }
 
-   @Override
+    @Override
     public void update() {
         // Si el sprite está siendo arrastrado, actualiza su posición según la posición del mouse
-        if (sprite.isDragging()) {
-            int mouseX = (int) Display.get().getMousePosition().getX();
-            int mouseY = (int) Display.get().getMousePosition().getY();
-            setPosition(new Vector2(mouseX, mouseY));
-        }
+//        if (sprite.isDragging()) {
+//            int mouseX = (int) Display.get().getMousePosition().getX();
+//            int mouseY = (int) Display.get().getMousePosition().getY();
+//            setPosition(new Vector2(mouseX, mouseY));
+//        }
     }
 
-    public int getSide1() {
-        return side1;
-    }
-
-    public int getSide2() {
-        return side2;
-    }
-
-    public Sprite getSprite() {
-        return sprite;
-    }
 }
