288831c861c9a74720ac22f9e404f07984119e8a
Hector Icedo <icedohector@gmail.com>
2023-10-13 16:57:40 -0700

    Se agregó la logica de la clase KeyListener

--- src/main/java/org/itson/view/KeyListener.java
+++ src/main/java/org/itson/view/KeyListener.java
@@ -2,18 +2,37 @@ package org.itson.view;
 
 import java.awt.event.KeyEvent;
 
-/**
- *
- * @author arace
- */
 public class KeyListener implements java.awt.event.KeyListener {
+
+    private boolean[] keys;
+
+    public KeyListener() {
+        keys = new boolean[256];
+    }
 
     @Override
     public void keyPressed(KeyEvent e) {
+        int keyCode = e.getKeyCode();
+        if (keyCode >= 0 && keyCode < keys.length) {
+            keys[keyCode] = true;
+        }
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
+        int keyCode = e.getKeyCode();
+        if (keyCode >= 0 && keyCode < keys.length) {
+            keys[keyCode] = false;
+        }
+    }
+
+
+    public boolean isKeyPressed(int keyCode) {
+        if (keyCode >= 0 && keyCode < keys.length) {
+            return keys[keyCode];
+        } else {
+            return false;
+        }
     }
 
     @Override
