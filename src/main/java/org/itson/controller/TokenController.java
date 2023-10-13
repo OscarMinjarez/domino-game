b753a4c5ea01ff757da7ab0052ec38c9ec694cd6
Hector Icedo <icedohector@gmail.com>
2023-10-12 20:51:51 -0700

    Se creo un metodo que permita la rotacion de las fichas

--- src/main/java/org/itson/controller/TokenController.java
+++ src/main/java/org/itson/controller/TokenController.java
@@ -4,6 +4,10 @@
  */
 package org.itson.controller;
 
+import org.itson.model.domain.Token;
+import org.itson.model.enums.TokenHorVer;
+import org.itson.model.enums.TokenPosition;
+
 /**
  *
  * @author luis-
@@ -22,5 +26,33 @@ public class TokenController {
         }
         return instance;
     }
+    
+    private void swapValuesBetweenFaces(Token.Face face1, Token.Face face2) {
+    int tempValue = face1.getValue();
+    face1.setValue(face2.getValue());
+    face2.setValue(tempValue);
+}
+
+public void rotateToken(Token token) {
+    if (token.getTokenHorVer() == TokenHorVer.HORIZONTAL) {
+        token.setTokenHorVer(TokenHorVer.VERTICAL);
+
+        // Cambiar las posiciones de las caras cuando el token es horizontal
+        token.getFaces()[0].setTokenPosition(TokenPosition.LEFT);
+        token.getFaces()[1].setTokenPosition(TokenPosition.RIGHT);
+
+        // Intercambiar los valores de las caras
+        swapValuesBetweenFaces(token.getFaces()[0], token.getFaces()[1]);
+    } else {
+        token.setTokenHorVer(TokenHorVer.HORIZONTAL);
+
+        // Cambiar las posiciones de las caras cuando el token es vertical
+        token.getFaces()[0].setTokenPosition(TokenPosition.TOP);
+        token.getFaces()[1].setTokenPosition(TokenPosition.BOTTOM);
+
+        // Intercambiar los valores de las caras
+        swapValuesBetweenFaces(token.getFaces()[0], token.getFaces()[1]);
+    }
+}
     
 }
--- src/main/java/org/itson/model/domain/Token.java
+++ src/main/java/org/itson/model/domain/Token.java
@@ -8,10 +8,11 @@ import org.itson.utils.Vector2;
 import org.itson.view.Sprite;
 
 public class Token extends Entity {
+
     private Face[] faces;
     private Vector2 position;
     private TokenHorVer tokenHorVer;
-    
+
     public Token(Sprite face1Sprite, int face1Valor, Sprite face2Sprite, int face2Valor, Vector2 position, TokenHorVer tokenHorVer, TokenPosition tokenPosition) {
         this.faces = new Face[2];
         this.faces[0] = new Face(face1Sprite, face1Valor, tokenPosition);
@@ -20,27 +21,71 @@ public class Token extends Entity {
         this.tokenHorVer = tokenHorVer;
     }
 
-    private class Face {
+    public class Face {
+
         private Sprite sprite;
-        private int valor;
+        private int value;
         private TokenPosition tokenPosition;
-        
+
         public Face(Sprite sprite, int valor, TokenPosition tokenPosition) {
             this.sprite = sprite;
-            this.valor = valor;
+            this.value = value;
             this.tokenPosition = tokenPosition;
         }
-        
-        public Face(){
-            
+
+        public Face() {
+
         }
 
         public Sprite getSprite() {
             return sprite;
         }
 
-        public int getValor() {
-            return valor;
+        public void setValue(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public void setTokenPosition(TokenPosition tokenPosition) {
+            this.tokenPosition = tokenPosition;
+        }
+
+    }
+
+    public Face[] getFaces() {
+        return faces;
+    }
+
+    public void setFaces(Face[] faces) {
+        this.faces = faces;
+    }
+
+    public TokenHorVer getTokenHorVer() {
+        return tokenHorVer;
+    }
+
+    public void setTokenHorVer(TokenHorVer tokenHorVer) {
+        this.tokenHorVer = tokenHorVer;
+    }
+
+    public void setTokenPosition(TokenPosition tokenPosition) {
+        this.faces[0].setTokenPosition(tokenPosition);
+
+        if (tokenHorVer == TokenHorVer.HORIZONTAL) {
+            if (tokenPosition == TokenPosition.TOP) {
+                this.faces[1].setTokenPosition(TokenPosition.RIGHT);
+            } else if (tokenPosition == TokenPosition.BOTTOM) {
+                this.faces[1].setTokenPosition(TokenPosition.LEFT);
+            }
+        } else {
+            if (tokenPosition == TokenPosition.LEFT) {
+                this.faces[1].setTokenPosition(TokenPosition.TOP);
+            } else if (tokenPosition == TokenPosition.RIGHT) {
+                this.faces[1].setTokenPosition(TokenPosition.BOTTOM);
+            }
         }
     }
 
